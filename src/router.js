b3fe6fd79d3bae922d1635d4f8a35d7e79965a04
uhmtoto <uhmtotow@gmail.com>
2019-08-06 21:27:09 +0900

    feat: implement forbid/required auth util

--- src/router.js
+++ src/router.js
@@ -4,6 +4,20 @@ import Home from './views/Home.vue'
 
 Vue.use(Router)
 
+function requireAuth (to, from, next) {
+  if (localStorage.token) {
+    axios.defaults.headers.common.authorization =
+      localStorage.token
+    return next()
+  }
+  next('/auth/login')
+}
+
+function forbidAuth (to, from, next) {
+  if (localStorage.token) return next('/')
+  next()
+}
+
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
@@ -11,7 +25,8 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      beforeEnter: forbidAuth
     }
   ]
 })
