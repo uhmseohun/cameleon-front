b1fb2d00fbcaa8ef3fb6a91e01f9c58fbca27463
uhmtoto <uhmtotow@gmail.com>
2019-08-07 00:35:13 +0900

    fix: some layout errors

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@ export default {
     <brand-bar
       v-show="showBars"
     />
-    <router-view />
+    <router-view class="app__content" />
     <menu-bar
       v-show="showBars"
       class="app__menubar"
@@ -28,9 +28,40 @@ export default {
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Gothic+A1&display=swap');
+
 a {
   text-decoration: none;
   color: black;
+}
+
+html,
+body {
+  height: 100%;
+}
+
+.app {
+  height: 100%;
+
+  &__content {
+    font-family: 'Gothic A1', sans-serif;
+    margin: 0 auto;
+    padding: 1rem 15px 0 15px;
+
+    @media (min-width: 650px) {
+      padding: 3rem 10rem 0 10rem;
+    }
+  }
+
+  &__menubar {
+    width: -webkit-fill-available;
+    display: flex;
+    justify-content: space-between;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
 }
 
 .input {
@@ -42,22 +73,18 @@ a {
     height: 2.3rem;
     padding-left: 1rem;
     font-size: 1rem;
-
     &::placeholder {
       font-size: 1rem;
     }
   }
-}
 
-.app {
-  &__menubar {
-    width: -webkit-fill-available;
-    display: flex;
-    justify-content: space-between;
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
+  &__button {
+    background-color: rgb(233, 233, 233);
+    border-radius: 15px;
+    border: 0;
+    height: 2.3rem;
+    padding-left: 1rem;
+    font-size: 1rem;
   }
 }
 </style>
