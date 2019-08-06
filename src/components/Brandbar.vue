6da49f8a3f7cec2f04d11199528b788f53c5c39d
uhmtoto <uhmtotow@gmail.com>
2019-08-06 23:23:16 +0900

    feat: new brand bar

---
+++ src/components/Brandbar.vue
@@ -0,0 +1,55 @@
+<script>
+export default {
+  name: 'brandbar',
+  methods: {
+    logout () {
+      delete localStorage.token
+      delete this.$api.defaults.headers.common['authorization']
+      this.$router.push('/auth/login')
+    }
+  }
+}
+</script>
+
+<template>
+<div class="navbar">
+  <div class="navbar__brand">
+    <span class="navbar__block">카멜레온</span>
+  </div>
+  <div class="navbar__logout">
+    <i
+      @click="logout()"
+      class="fa fa-sign-out"
+    />
+  </div>
+</div>
+</template>
+
+<style lang="scss" scoped>
+.navbar {
+  height: 1.5rem;
+  margin: 17px 15px;
+
+  &__brand {
+    float: left;
+    font-size: 1.3rem;
+    user-select: none;
+  }
+
+  &__block {
+    display: block;
+  }
+
+  &__logout {
+    float: right;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    font-size: 1.7rem;
+  }
+
+  &__icon {
+    height: 2rem;
+  }
+}
+</style>
