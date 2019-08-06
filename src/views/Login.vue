a58fb2df666031ad710eef659d836f39958e318a
uhmtoto <uhmtotow@gmail.com>
2019-08-07 08:46:12 +0900

    fix: media query max width

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -96,7 +96,7 @@ export default {
     border-radius: 20px;
     box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
 
-    @media (max-width: 414px) {
+    @media (max-width: 950px) {
       width: 80vw;
     }
 
