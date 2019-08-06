46f51d6ecf4886d74ed18977648d3363cbe67f7d
uhmtoto <uhmtotow@gmail.com>
2019-08-07 00:35:48 +0900

    feat: color item component

---
+++ src/components/ColorItem.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: 'coloritem',
+  props: {
+    color: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+<div
+  class="color"
+  :style="{
+    'background-color': `#${color.rgb}`
+  }"
+>
+  <span class="color__name">{{ color.name }}</span>
+  <span class="color__intro">{{ color.introduce }}</span>
+</div>
+</template>
+
+<style lang="scss" scoped>
+.color {
+  vertical-align: middle;
+  height: 3rem;
+  border-radius: 8px;
+  padding: 10px;
+
+  &__name {
+    display: block;
+    font-size: 1.2rem;
+  }
+}
+</style>
