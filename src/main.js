64ab4a828d5a40cad9d85f18729a5fea88890029
uhmtoto <uhmtotow@gmail.com>
2019-08-06 21:19:36 +0900

    feat: initialize swal and axios

--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import vueSwal from 'vue-swal'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$api = axios.create({
+  baseURL: 'http://localhost:3000'
+})
+
+Vue.use(vueSwal)
 
 new Vue({
   router,
