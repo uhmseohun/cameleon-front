f0bffd9c7af16a0cacf6b0c9260cda28a8ed54e0
uhmtoto <uhmtotow@gmail.com>
2019-08-06 21:19:23 +0900

    feat: input style

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,23 @@
 </template>
 
 <style lang="scss">
+a {
+  text-decoration: none;
+}
+
+.input {
+  &__text {
+    background-color: rgb(233, 233, 233);
+    border-radius: 15px;
+    border: 0;
+    outline: none;
+    height: 2.3rem;
+    padding-left: 1rem;
+    font-size: 1rem;
+
+    &::placeholder {
+      font-size: 1rem;
+    }
+  }
+}
 </style>
