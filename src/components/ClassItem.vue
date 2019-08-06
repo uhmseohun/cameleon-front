21d565a25376c7df86585fc7249749a7b0e4c822
uhmtoto <uhmtotow@gmail.com>
2019-08-07 01:13:58 +0900

    feat: class item component

---
+++ src/components/ClassItem.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: 'classitem',
+  props: {
+    klass: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+<div class="klass">
+  <span class="klass__name">{{ klass.name }}</span>
+  <span class="klass__intro">{{ klass.introduce }}</span>
+</div>
+</template>
+
+<style lang="scss" scoped>
+.klass {
+  vertical-align: middle;
+  height: 3rem;
+  border-radius: 8px;
+  padding: 10px;
+  background-color: rgb(233, 233, 233);
+
+  &__name {
+    display: block;
+    font-size: 1.2rem;
+  }
+}
+</style>
