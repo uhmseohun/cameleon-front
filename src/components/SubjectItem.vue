3df1f9b19b19b87e8113607275637d37db62c57c
uhmtoto <uhmtotow@gmail.com>
2019-08-07 02:40:43 +0900

    feat: subject item component

---
+++ src/components/SubjectItem.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  name: 'subjectitem',
+  props: {
+    subject: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      color: 'rgb(233, 233, 233)'
+    }
+  }
+}
+</script>
+
+<template>
+<div
+  class="subject"
+  :style="{
+    'background-color': `rgb()`
+  }"
+>
+  <span class="subject__name">{{ subject.name }}</span>
+  <span class="subject__intro">{{ subject.introduce }}</span>
+</div>
+</template>
+
+<style lang="scss" scoped>
+.subject {
+  display: flex;
+  align-items: center;
+  height: 3rem;
+  border-radius: 8px;
+  padding: 10px;
+  background-color: rgb(233, 233, 233);
+
+  &__name {
+    display: block;
+    font-size: 1.2rem;
+  }
+}
+</style>
